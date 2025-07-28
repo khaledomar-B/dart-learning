26456fffa7df9b61603be9be1510f8d84da01985
khaled omar <ko917547@gmail.com>
2025-07-29 02:20:45 +0300

    13. File structure

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("hello worid !!");
+}
+
+//When we run the side bar, it displays the entire project, which consists of several files.
+//In order to run from the side bar, we need to activate the lib folder and put the project files inside it.
+//If we need to run from the terminal, we type dart + the path to the file.
+/*ctrl+shift+p --> command palette --> dart:use recommended --> It contains a group of settings
+ that will help you while writing, and in the future you can add settings to this place.
+ */
