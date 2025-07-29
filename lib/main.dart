a65d1e487639f8c4d157dfa7643dc95cd7c9fd2a
khaled omar <ko917547@gmail.com>
2025-07-29 13:20:10 +0300

    Variables part 1+2+3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
-void main() {
-  print("hello worid !!");
-}
+//Some uses of variables
 
-//When we run the side bar, it displays the entire project, which consists of several files.
-//In order to run from the side bar, we need to activate the lib folder and put the project files inside it.
-//If we need to run from the terminal, we type dart + the path to the file.
-/*ctrl+shift+p --> command palette --> dart:use recommended --> It contains a group of settings
- that will help you while writing, and in the future you can add settings to this place.
- */
+// ignore_for_file: unused_local_variable
+
+/*Variables in programming are the basis for temporarily storing data 
+within the program during its execution.*/
+//We use variables to reuse a value more than once.
+//To update the value while the program is running
+//To store user input
+
+//create variable--> 1.data type--->2.name variable = The name of the place where the value is stored
+/*
+Some rules for naming a variable:
+
+1. The name must begin with a letter or a symbol (_) only.
+2. It can contain letters, numbers, or a _, but must not begin with a number or a _.
+3. It must not be the same name as a Dart reserved word.
+*/
+
+void main() {
+  String name;
+  name = 'khaled';
+  //memory[]= -->name
+  print('my name is $name');
+  print('my name is $name');
+  print('my name is $name');
+  print('my name is $name');
+}
